fcf3d8ad1d77f8bfaa41e9b4eb367ef165f7709e
NarminBagirova <nnarminbagirova@gmail.com>
2024-10-12 16:45:03 +0400

    InitializedProject

---
+++ Task1_Task2/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Task1_Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1
+            //bool isFirst = true;
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % 3 == 0 && i % 5 == 0)
+            //    {
+            //        if (!isFirst)
+            //        {
+            //            Console.Write(", ");
+            //        }
+            //        Console.Write(i);
+            //        isFirst = false;
+
+            //    }
+            //}
+            #endregion
+
+            #region Task2
+            //int number = 96;
+
+            //if (number < 65)
+            //{
+            //    Console.WriteLine("Kesildiniz");
+            //}
+
+            //else if (number >= 65 && number < 85)
+            //{
+            //    Console.WriteLine("Adi diplom");
+            //}
+
+            //else if(number>=85 && number<95)
+            //{
+            //    Console.WriteLine("Sheref diplomu (Honour)");
+
+            //}
+
+            //else(number>=95 && number<=100)
+            //{
+            //    Console.WriteLine("Yuksek sheref diplomu (High Honour)");
+            //}
+            #endregion
+        }
+    }
+}
